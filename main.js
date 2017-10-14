6a62eba1e87ce6d133604fbaaa429ef3ebd9989b
Uri Shaked <uri@salsa4fun.co.il>
2017-10-15 02:10:51 +0300

    Blink demo

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
-const { MuseClient } = require('muse-js');
+const { MuseClient, channelNames } = require('muse-js');
+const { Observable } = require('rxjs/Observable');
+require('rxjs/add/observable/timer');
+require('rxjs/add/observable/of');
+require('rxjs/add/operator/switchMap');
+require('rxjs/add/operator/distinctUntilChanged');
 
-console.log('Write your code here');
+const eye = document.querySelector('.eye');
+
+let client = new MuseClient();
+async function connect() {
+  await client.connect();
+  await client.start();
+  const leftEye = channelNames.indexOf('AF7');
+  console.log('Connected!');
+
+  client.eegReadings
+    .filter(data => data.electrode === leftEye)
+    .map(data => Math.max(...data.samples.map(Math.abs)))
+    .filter(value => value > 150)
+    .switchMap(stam => Observable.merge(
+      Observable.of(true),
+      Observable.timer(300).map(() => false)
+    ))
+    .distinctUntilChanged()
+    .subscribe(blink => {
+      if (blink) {
+        eye.classList.add('blink');
+      } else {
+        eye.classList.remove('blink');
+      }
+    })
+}
+
+window.connect = connect;
